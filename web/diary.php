2613e6281c28e1cfdade0cc52eeb3eebde82ceda
eugenekkim8 <36252706+eugenekkim8@users.noreply.github.com>
2021-08-04 11:13:20 -0400

    Post-redirect-get on diary

--- web/diary.php
+++ web/diary.php
@@ -1,3 +1,44 @@
+<?php
+    // post-redirect-get mechanism to avoid double entries
+    // form can be submitted only if p is valid, so no need to redo verification
+
+    // establish connection
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+
+    $connect_str = "host = " . $dbopts["host"] . " port = " . $dbopts["port"] . " dbname = " . ltrim($dbopts["path"], "/") . " user = " . $dbopts["user"] . " password = " . $dbopts["pass"];
+
+    $conn = pg_connect($connect_str) or die("Could not connect" . pg_last_error());
+
+    $alert_text = "";
+
+    // switch subscription status if user has requested
+    if (isset($_POST["toggleSubscribe"])){
+        $query = "UPDATE users SET is_active = NOT(is_active) WHERE password = $1";
+        $results = pg_query_params($conn, $query, array($_GET["p"])) or die ("Query failed:" . pg_last_error());
+
+        $query = "SELECT is_active FROM users WHERE password = $1";
+        $results = pg_query_params($conn, $query, array($_GET["p"])) or die ("Query failed:" . pg_last_error());
+        
+        $this_user = pg_fetch_array($results); // only one user should be returned because password must be UNIQUE
+        $msg_text = ($this_user["is_active"] == 't') ? 'unsubscribed' : 'subscribed';
+        $alert_text = 'You have successfully ' . $msg_text . '!';
+    }
+
+    // if user has clicked on submit button
+    if (isset($_POST["submitButton"])){
+        
+        $query = "INSERT INTO diaries (password, diary_ts, score, comment, local_date) VALUES ($1, NOW(), $2, $3, $4)";
+        $results = pg_query_params($conn, $query, array($_GET["p"], $_POST["score"], $_POST["comment"], $_POST["local_date"])) or die ("Query failed:" . pg_last_error());
+
+        $alert_text = 'Entry submitted!';
+    }
+
+    header("Location: " . $_SERVER['REQUEST_URI'] . "&alert_text=" . $alert_text); 
+    exit();
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -50,13 +91,6 @@
         <?php
 
             if (isset($_GET["p"])){
-                
-                // establish connection
-                $dbopts = parse_url(getenv('DATABASE_URL'));
-
-                $connect_str = "host = " . $dbopts["host"] . " port = " . $dbopts["port"] . " dbname = " . ltrim($dbopts["path"], "/") . " user = " . $dbopts["user"] . " password = " . $dbopts["pass"];
-
-                $conn = pg_connect($connect_str) or die("Could not connect" . pg_last_error());
 
                 // see if there is a user with password = p, and if so, subscription status
 
@@ -72,18 +106,7 @@
                 } else { // otherwise, display submit button and (un)subscribe button
                     
                     $this_user = pg_fetch_array($results); // only one user should be returned because password must be UNIQUE
-
                     $user_active = ($this_user["is_active"] == 't') ? TRUE : FALSE;
-                    $alert_text = "";
-
-                    // switch subscription status if user has requested
-                    if (isset($_POST["toggleSubscribe"])){
-                        $query = "UPDATE users SET is_active = NOT(is_active) WHERE password = $1";
-                        $results = pg_query_params($conn, $query, array($_GET["p"])) or die ("Query failed:" . pg_last_error());
-                        $msg_text = $user_active ? 'unsubscribed' : 'subscribed';
-                        $alert_text = 'You have successfully ' . $msg_text . '!';
-                        $user_active = !$user_active;
-                    }
 
                     echo '<input type="hidden" name="p" value="' . $_GET["p"] . '"> <button type="submit" class="btn btn-primary" name="submitButton" value="set">Submit</button> ';
 
@@ -91,17 +114,9 @@
 
                     echo '<button type="submit" class="btn btn-outline-secondary" name="toggleSubscribe" value="set">' . $button_text . '</button>';
 
-                    // if user has clicked on submit button
-                    if (isset($_POST["submitButton"])){
-                        
-                        $query = "INSERT INTO diaries (password, diary_ts, score, comment, local_date) VALUES ($1, NOW(), $2, $3, $4)";
-                        $results = pg_query_params($conn, $query, array($_GET["p"], $_POST["score"], $_POST["comment"], $_POST["local_date"])) or die ("Query failed:" . pg_last_error());
-
-                        $alert_text = 'Entry submitted!';
-                    }
-
-                    if ($alert_text != ""){
-                        echo '<div class="alert alert-success alert-dismissible fade show mt-3" role="alert">' . $alert_text . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+                    
+                    if (isset($_GET["alert_text"])){
+                        echo '<div class="alert alert-success alert-dismissible fade show mt-3" role="alert">' . $_GET["alert_text"] . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
                     }
 
                 }
