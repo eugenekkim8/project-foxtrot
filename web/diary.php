511c50bfdaaafb839e13b81fbb654cff3b9c0105
eugenekkim8 <36252706+eugenekkim8@users.noreply.github.com>
2021-10-31 22:28:23 -0400

    Removing echo on mail PRG

--- web/diary.php
+++ web/diary.php
@@ -141,7 +141,6 @@
 
         try {
             $mail->send();
-            echo 'Message has been sent';
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
